733b1f3aa7fc077edcbc79b981699ecc2661da70
Emmanuel Miras <prezolov@live.com>
2017-03-04 16:45:55 +0000

    Gui allows for collapsable poses, can now save and stylize multiple poses, either one by one or at the same time.

--- Procedural animation stylization/Procedural animation stylization/main.py
+++ Procedural animation stylization/Procedural animation stylization/main.py
@@ -1,10 +1,10 @@
 import maya.cmds as cmds
 import maya.mel as mel
 import math
-initIW = []
 framesPosed = []
-init = True
-next = 0 # Holds the value of the next keyframe
+init = []#True
+nextLoop = []
+next = [] # Holds the value of the next keyframe
 # https://www.desmos.com/calculator/hamwkcp1rh
 def calculatePos(x, n):
     #decimal.normalize(x)
@@ -24,8 +24,6 @@ def SavePoseButtonPush(*args):
     if len(selected) < 1:
         cmds.warning("No object selected.")
     else:
-        global initIW
-        initIW = cmds.keyTangent(cmds.ls(sl=1), query = True, iw = 1)
         #print(initIW)
         print("Pose saved...")
         
@@ -39,16 +37,15 @@ def SavePoseButtonPush(*args):
                 alreadyPosed = True
         
         if alreadyPosed == False:
-            # Add current frame to the list of frames that have been posed
-            
-            framesPosed.insert(0, cmds.currentTime(query=True))
+            # Add current frame to the array of frames that have been posed
+            # Adds to end of array
+            framesPosed.insert(len(framesPosed), cmds.currentTime(query=True))
             print(framesPosed)
             #cmds.text( label='Pose at frame: ' + str(cmds.currentTime( query=True )), bgc=[1,0,0])
-            cmds.button( label='Pose at frame: ' + str(cmds.currentTime( query=True )), command=SavePoseButtonPush)
+            #cmds.button( label='Pose at frame: ' + str(cmds.currentTime( query=True )), command=SavePoseButtonPush)
             #cmds.columnLayout( adjustableColumn=True )
-            #cmds.frameLayout( label='Pose at frame: ' + str(cmds.currentTime( query=True )), labelAlign='top', borderStyle='in' )
-            #cmds.columnLayout()
-
+            cmds.checkBox('stylize' + str(int(cmds.currentTime( query=True ))),  label='Stylize', bgc=[0.75,0.7,0.7] )
+            cmds.frameLayout('pose' + str(int(cmds.currentTime( query=True ))), label='Pose at frame: ' + str(cmds.currentTime( query=True )), labelAlign='top', cll = True, cl = True )
             keyable = cmds.listAttr(cmds.ls(sl=1), k=True)
             for i in range(0, len(keyable)):
                 cmds.checkBox( label=keyable[i] )
@@ -59,58 +56,45 @@ def SavePoseButtonPush(*args):
             keyable = cmds.listAttr( cmds.ls(sl=1), k=True)  
             for i in range(0, len(keyable)):
                 cmds.textField(keyable[i] +"_"+ str(int(cmds.currentTime( query=True ))), edit = True, text = str(cmds.getAttr(cmds.ls(sl=1)[0] + "." + keyable[i])))
-
-def ResetButtonPush(*args):
-    print("Resetting the values")
-    print(initIW)
-    for i in range(len(initIW)):
-        print(i)
-        cmds.keyTangent(cmds.ls(sl=1), edit = True, index = (i, i), iw = float(initIW[i]))
+        cmds.setParent( '..' )
 
 def slider_drag_callback(*args):
     print("Slider Dragged")
     global framesPosed
     print(framesPosed)
-    currentKeyFrame = framesPosed[0] #cmds.currentTime( query=True )
-    global next
-    global init
-    if init == True: 
-        next = cmds.findKeyframe(cmds.ls(sl=1), time=(framesPosed[0],framesPosed[0]), which="next")
-        init = False
-    #previous = cmds.findKeyframe(cmds.ls(sl=1), time=(framesPosed[0],framesPosed[0]), which="previous")
-    #key1 = 0
-    #if (len(framesPosed) > 0):
-    
-    # Put keyframe inbetween
-   # key1 = (currentKeyFrame + next) / 2
-    # Put keyframe one after current
-    key2 = currentKeyFrame + 1 #(currentKeyFrame + key1) / 2
-    # Put keyframe one before next
-    key3 = next - 1 #(key1 + next) / 2
+     #cmds.currentTime( query=True )
+    for i in range(0, len(framesPosed)):
+        currentKeyFrame = framesPosed[i]
+        if cmds.checkBox('stylize' + str(int(framesPosed[i])), q = True, value = True) == True:
+            global next
+            global init
+            print(i)
+            print(len(init))
+            if(i == len(init)):
+                init.insert(i, True)
 
-    valueFromSlider = cmds.floatSliderGrp('float', query=True, value = 1)
-    print("value from slider is: " + str(valueFromSlider))
-    # print(calculatePos(key1, valueFromSlider))
-    #cmds.setKeyframe( cmds.ls(sl=1), at = 'translateY', v=calculatePos(key1, valueFromSlider), t = (key1, key1), itt = "spline", ott = "spline" )
-    cmds.setKeyframe( cmds.ls(sl=1), at = 'translateY', v=calculatePos(key2, valueFromSlider), t = (key2, key2), itt = "spline", ott = "spline" )
-    cmds.setKeyframe( cmds.ls(sl=1), at = 'translateY', v=calculatePos(key3, valueFromSlider), t = (key3, key3), itt = "spline", ott = "spline" )
-    #cmds.setAttr()
-    #cmds.keyTangent(cmds.ls(sl=1), edit = True, time = (previous, next), attribute = 'translateY', outWeight = valueFromSlider * 10)
-    #cmds.keyTangent(cmds.ls(sl=1), edit = True, time = (previous, next), attribute = 'translateY', inWeight = valueFromSlider * 10)
-    #cmds.keyTangent(cmds.ls(sl=1), edit = True, time = (currentKeyFrame, currentKeyFrame), attribute = 'translateY', itt = 'flat', ott = 'flat')
-    #if valueFromSlider==1.0:
+            if init[i] == True: 
+                next.insert(i, cmds.findKeyframe(cmds.ls(sl=1), time=(currentKeyFrame,currentKeyFrame), which="next"))
+                init[i] = False
+            # Put keyframe one after current
+            key2 = currentKeyFrame + 1 #(currentKeyFrame + key1) / 2
+            # Put keyframe one before next
+            key3 = next[i] - 1 #(key1 + next) / 2
+
+            valueFromSlider = cmds.floatSliderGrp('float', query=True, value = 1)
+            print("value from slider is: " + str(valueFromSlider))
+            cmds.setKeyframe( cmds.ls(sl=1), at = 'translateY', v=calculatePos(key2, valueFromSlider), t = (key2, key2), itt = "spline", ott = "spline" )
+            cmds.setKeyframe( cmds.ls(sl=1), at = 'translateY', v=calculatePos(key3, valueFromSlider), t = (key3, key3), itt = "spline", ott = "spline" )
+
 
 # Make a new window
 window = cmds.window( title="Animation Stylization", iconName='Short Name', widthHeight=(500, 500) )
 # Assign a layout
 cmds.columnLayout( adjustableColumn=True )
-
 # Add a button
 cmds.button( label='Save Pose', command=SavePoseButtonPush)
 
-cmds.floatSliderGrp('float', label='Cartoonification Value', field=True, minValue=-10.0, maxValue=10.0, fieldMinValue=-10.0, fieldMaxValue=10.0, value=0, dc=slider_drag_callback)
-# Add a button
-cmds.button( label='Reset', command=ResetButtonPush)
+cmds.floatSliderGrp('float', label='Stylization Value', field=True, minValue=-10.0, maxValue=10.0, fieldMinValue=-10.0, fieldMaxValue=10.0, value=0, dc=slider_drag_callback)
 
 # Show the window
 cmds.showWindow( window )
